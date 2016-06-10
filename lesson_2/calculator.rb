2cd61b44d04b7e0732160efe40a14224a12745c5
oschebell <oschebell@gmail.com>
2016-06-10 22:37:55 +1000

    Add messages to external yaml configuration file

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,39 +1,45 @@
-#ask the user for two numbers
-#ask the user for an operation to perform
-#perform the operation on the two numbers
-#output the result
+# ask the user for two numbers
+# ask the user for an operation to perform
+# perform the operation on the two numbers
+# output the result
 
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+LANGUAGE = 'en'
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
 
 def prompt(message)
   puts("=> #{message}")
 end
 
 def valid_number?(num)
-  num.to_i != 0
+  num.to_i.to_s == num
 end
 
 def operation_to_message(operation)
   word = case operation
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
-
+         when '1'
+           'Adding'
+         when '2'
+              'Subtracting'
+         when '3'
+              'Multiplying'
+         when '4'
+              'Dividing'
+       end
 end
 
-prompt("Welcome to calculator. Please tell me your name.")
+prompt(messages('welcome'))
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt("Please enter a valid_name.")
+    prompt(messages('valid_name'))
   else
     break
   end
@@ -45,25 +51,25 @@ loop do # main loop
   number1 = ''
 
   loop do # internal loop 1
-    prompt("What is the first number?")
+    prompt(messages('first_number'))
     number1 = gets.chomp
 
     if valid_number?(number1)
       break
     else
-      prompt("Hmm...that doesn't look right. Please enter a valid number.")
+      prompt(messages('invalid_number'))
     end
   end
 
   number2 = ''
   loop do # internal loop 2
-    prompt("What is the second number?")
+    prompt(messages('second_number'))
     number2 = gets.chomp
 
     if valid_number?(number2)
       break
     else
-      prompt("Hmm...that doesn't look right. Please enter a valid number.")
+      prompt(messages('invalid_number'))
     end
   end
 
@@ -91,21 +97,21 @@ loop do # main loop
   prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
-            when '1'
-              number1.to_i + number2.to_i
-            when '2'
-              number1.to_i - number2.to_i
-            when '3'
-              number1.to_i * number2.to_i
-            when '4'
-              number1.to_f / number2.to_f
-          end
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
 
   prompt("The result is #{result}")
 
-  prompt("Would you like to perform another calculation?")
+  prompt(messages('perform_again'))
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thank you for using calculator. Bye!")
+prompt(messages('goodbye'))
